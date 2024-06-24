22537ea8026f8513affbaec2db59f569b8e28e04
EizeneimNout <EizeneimNout@DESKTOP-BS2RGGR>
2024-06-24 17:34:51 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Lesson3__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 43;
+            int res = 5 * 43;
+
+
+            Console.Write("Число ");
+            Console.Write(num1);
+            Console.Write(" помножити на ");
+            Console.Write(num2);
+            Console.Write(" дорівнює ");
+            Console.Write(res + "\n");
+
+            Console.WriteLine("Число " + num1 + " помножити на " + num2 + " дорівнює " + res);//контактенація
+
+            Console.WriteLine($"Число {num1*num2} помножити на {num2} дорівнює {res}.");//інтерполяція.
+
+
+        }
+    }
+}
